01a81f137b6003436b451235952bc00730824ca1
unknown <Smurugan@mvni424855.myer.internal>
2014-04-07 11:53:48 +1000

    first check in with maven

---
+++ src/main/java/com/myer/ImageRender/App.java
@@ -0,0 +1,13 @@
+package com.myer.ImageRender;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
